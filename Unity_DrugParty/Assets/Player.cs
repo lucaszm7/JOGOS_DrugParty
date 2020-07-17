7db488416b160cd3e94b0eb7f882fb129dc2ae6e
Lucas Morais <lucas.morais446@hotmail.com>
2020-07-17 17:55:41 -0300

    Add a floor and a player

---
+++ Unity_DrugParty/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D physics;
+    void Awake()
+    {
+        this.physics = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.physics.AddForce(Vector3.right * 5);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.physics.AddForce(Vector3.left * 5);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.physics.AddForce(Vector3.up * 5, ForceMode2D.Impulse);
+        }
+    }
+}
