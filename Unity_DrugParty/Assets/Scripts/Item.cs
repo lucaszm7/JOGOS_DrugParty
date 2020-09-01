6af6552187fb5888211d0e281e090c6d3d451a5b
Lucas Alviene <lucasalp1@hotmail.com>
2020-09-01 16:16:56 -0300

    Separei o LevelController e GameController

--- Unity_DrugParty/Assets/Scripts/Item.cs
+++ Unity_DrugParty/Assets/Scripts/Item.cs
@@ -6,7 +6,7 @@ public class Item : MonoBehaviour{
 
     void OnCollisionEnter2D(Collision2D collision){
     	if(collision.gameObject.tag == "Player"){
-    		GameObject.FindWithTag("GameController").GetComponent<GameController>().addItem(this);
+    		GameObject.FindWithTag("GameController").GetComponent<LevelController>().addItem(this);
             gameObject.SetActive(false);
             //Destroy(gameObject);
     	}
