87436dd2480a55de445b3a28beadecaa18884fa4
Lucas Alviene <lucasalp1@hotmail.com>
2020-09-01 14:42:02 -0300

    Vida

---
+++ Unity_DrugParty/Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour{
+
+	const float width = 0.4455351f;
+	const float height = 0.08772884f;
+
+	public GameObject Red;
+
+	public int maxHP = 100;
+	public int currentHP = 1;
+
+	SpriteRenderer spriteRenderer; 
+
+    void Start()
+    {//0.4455351
+    	spriteRenderer = Red.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        spriteRenderer.size = new Vector2(currentHP/maxHP*width,height);
+    }
+
+}
